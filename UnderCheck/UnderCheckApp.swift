157409879aa3d684fec19ac7a3c83ba71d9502ef
Anastasia Boriskina <a.boriskina@outlook.com>
2023-01-16 12:54:55 +0500

    Initial Commit

---
+++ UnderCheck/UnderCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderCheckApp.swift
+//  UnderCheck
+//
+//  Created by Анастасия on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnderCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
